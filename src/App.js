10ef5059652f326f8a3854eb26f6c243c0c9b3e4
Sufyan Ahmed <sufiyan033130@gmail.com>
2023-08-27 14:43:15 +0500

    assigment lofic has been done

--- src/App.js
+++ src/App.js
@@ -1,38 +1,50 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App(props) {
+  const { wheatherdata } = props;
+  const [data, setData] = useState([]);
 
-  const {wheatherdata} = props;
-  console.log(wheatherdata)
+  useEffect(() => {
+    if (wheatherdata) {
+      setData((prevData) => [...prevData, wheatherdata]);
+    }
+    
+  }, [wheatherdata]);
+  useEffect(() => {
+      data.reverse();
+  }, [data]);
+
   return (
     <>
       <div className="container-fluid px-1 px-md-4 mx-auto">
-        <div className="row d-flex justify-content-center px-3">
-          <div className="card animate-card">
-            <h2 className="ml-auto mr-4 mt-3 mb-0">{(wheatherdata === null )? "City Name" : wheatherdata?.data?.location?.name}</h2>
-            <p className="ml-auto mr-4 mb-0 med-font">{(wheatherdata === null) ? "condition" : wheatherdata?.data?.current?.condition?.text} </p>
-            <h1 className="ml-auto mr-4 large-font">{(wheatherdata === null)? "temp_c":wheatherdata?.data?.current?.temp_c}&#176;</h1>
-            <p className="time-font mb-0 ml-4 mt-auto">
-              
-              
-              {(wheatherdata === null)? "Country" :                 
-              wheatherdata?.data?.location?.country
-              
-              }
-            
-            
-            
-            {/* <span className="sm-font">AM</span> */}
-            </p>
-            <p className="ml-4 mb-4">{(wheatherdata === null)? "Country Time" :                 
-              wheatherdata?.data?.location?.localtime
-              
-              }</p>
-          </div>
+       
+          {data.map((item, index) => (
+             <div className="row d-flex justify-content-center px-3"key={index}>
+            <div className="card animate-card item-center" >
+              <h2 className="ml-auto mr-4 mt-3 mb-0">
+                {item?.data?.location?.name}
+              </h2>
+              <p className="ml-auto mr-4 mb-0 med-font">
+                {item?.data?.current?.condition?.text}
+              </p>
+              <h1 className="ml-auto mr-4 large-font">
+                { item?.data?.current?.temp_c}
+                &#176;
+              </h1>
+              <p className="time-font mb-0 ml-4 mt-auto">
+                {item?.data?.location?.country}
+
+                {/* <span className="sm-font">AM</span> */}
+              </p>
+              <p className="ml-4 mb-4">
+                {item?.data?.location?.localtime}
+              </p>
+            </div>
         </div>
+
+          )).reverse()}
       </div>
-
-
     </>
   );
 }
--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -3,27 +3,33 @@ import axios from "axios";
 import "./input.css";
 import App from "../App";
 function Input() {
-  const [getinput, setgetinput] = useState("");
-  const iValue = useRef(null);
+  let iValue = useRef(null);
   const [apiData , setApiData] = useState(null);
+  const [err , setErr] = useState("")
   const callApi = async (e) => {
     e.preventDefault();
-    console.log(getinput);
+    // console.log(getinput);
     // console.log(iValue.current.value);
     let cityName = iValue?.current?.value;
     const BASE_URL = "https://api.weatherapi.com/v1/forecast.json?key=";
     const APIKEY = "09e23a81c17e423093b64454230607";
     try {
-      const res = await axios.get(`${BASE_URL}${APIKEY}&q=${cityName}&aqi=no`);
-      setApiData(res)
-      console.log(res);
+      if(cityName === "" ){
+          setErr("City Name Required!")
+      }else{
+        const res = await axios.get(`${BASE_URL}${APIKEY}&q=${cityName}&aqi=no`);
+        setApiData(res)
+        iValue.current.value = ""
+        setErr("")
+      }
+   
+      
+      // console.log(res);
     } catch (e) {
-      console.log(e);
+      console.log(e.response.data.error.message);
+      setErr(e.response.data.error.message)
     }
   };
-  function onchangeHandler(e) {
-    setgetinput(e.target.value);
-  }
   return (
     <>
       <div className="container sticky-top">
@@ -35,7 +41,6 @@ function Input() {
             maxLength={20}
             minLength={2}
             required
-            onChange={onchangeHandler}
             ref={iValue}
           />
           <button type="button" className="btn btn-primary" onClick={callApi}>
@@ -43,6 +48,7 @@ function Input() {
           </button>
         </div>
       </div>
+      <p className="text-danger text-center fw-bold">{err}</p>
       <App wheatherdata ={apiData}></App>
     </>
   );
